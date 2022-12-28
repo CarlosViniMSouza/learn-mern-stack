9ce23d10b2151a6a5e4663de51111448bdd88acf
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-28 19:15:04 -0300

    first commit

--- autogit.sh
+++ autogit.sh
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin <link>
+git remote add origin https://github.com/CarlosViniMSouza/learn-mern-stack.git
 git push -u origin main
 git status
