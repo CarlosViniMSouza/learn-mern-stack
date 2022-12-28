c48013af7c18f1dd3a3ae8b8d4c6dea64c32d128
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-12-28 19:16:26 -0300

    Install MongoDB Compass

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,6 @@
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/learn-mern-stack.git
-git push -u origin main
+git commit -m "Install MongoDB Compass"
+git push
 git status
+
+# stop moment: 37:00
